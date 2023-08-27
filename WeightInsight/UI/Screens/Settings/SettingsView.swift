3eea24d97f8077afa67a1bbb9b3bbb945e777ecd
Andrei Tekhtelev <andrei.tekhtelev@fortify.pro>
2023-08-26 22:28:41 -0700

    Fixed settings tab

--- WeightInsight/UI/Screens/Settings/SettingsView.swift
+++ WeightInsight/UI/Screens/Settings/SettingsView.swift
@@ -15,36 +15,71 @@ struct SettingsView: View {
 
     var body: some View {
         Form {
-            Section(header: Text("Your Goals").font(.system(size: 20))) {
+            Section(header: Text("Your Goals").font(.system(size: 17))) {
                 HStack {
                     Text("Weight:")
-                        .padding(10)
+                    Spacer()
                     CustomTextField(text: $viewModel.goals.weightGoal, placeholder: "Enter weight", keyboardType: .decimalPad, onDone: { value in
                         viewModel.saveSettingValue(value, for: .weight)
                     })
+                    .frame(width: 100)
                 }
+                 
                 HStack {
                     Text("Steps:")
-                        .padding(10)
+                    Spacer()
                     CustomTextField(text: $viewModel.goals.stepsGoal, placeholder: "Enter steps", keyboardType: .numberPad, onDone: { value in
                         viewModel.saveSettingValue(value, for: .steps)
                     })
-                }
+                    .frame(width: 100)
+                } 
                 HStack {
                     Text("Calories:")
-                        .padding(10)
+                    Spacer()
                     CustomTextField(text: $viewModel.goals.caloriesGoal, placeholder: "Enter calories", keyboardType: .numberPad, onDone: { value in
                         viewModel.saveSettingValue(value, for: .calories)
                     })
+                    .frame(width: 100)
                 }
+            }
+            .padding(EdgeInsets(top: 15, leading: 0, bottom: 15, trailing: 0))
+            .frame(height: 40)
+        
+            Section(header: Text("Connected apps").font(.system(size: 17))) {
                 HStack {
                     Text("MyFitnessPal:")
-                        .padding(10)
+                    Spacer()
                     Button(action: {
                         self.viewModel.showWebView = true
                     }) {
                         Text("Connect")
-                            .padding()
+                            .padding(10)
+                            .foregroundColor(.white)
+                            .background(Color.green)
+                    }
+                    .sheet(isPresented: $viewModel.showWebView) {
+//                        WebView(requestURL: URL(string: "https://www.myfitnesspal.com/food/diary/atekhtelev")!) { code in
+//                           self.authorizationCode = code
+//                           self.showWebView = false
+//                        }
+                        WebView(requestURL: URL(string: "http://api.myfitnesspal.com/v2/oauth2/auth?client_id=atekhtelev&response_type=code&scope=diary+measurements+private-exercises+subscriptions&redirect_uri=http://weightinsight.com/settings")!) { code in
+                            viewModel.authorizationCode = code
+                            viewModel.showWebView = false
+                        }
+                    }
+                    .cornerRadius(10)
+                    .disabled(true)
+                }
+                .padding(EdgeInsets(top: 15, leading: 0, bottom: 15, trailing: 0))
+                .frame(height: 40)
+                HStack() {
+                    Text("Fat Secret:")
+                    Spacer()
+                    Button(action: {
+                        self.viewModel.showWebView = true
+                    }) {
+                        Text("Connect")
+                            .padding(10)
                             .foregroundColor(.white)
                             .background(Color.green)
                     }
@@ -62,6 +97,8 @@ struct SettingsView: View {
                     .disabled(true)
                 }
             }
+            .padding(EdgeInsets(top: 15, leading: 0, bottom: 15, trailing: 0))
+            .frame(height: 40)
         }
     }
 }
